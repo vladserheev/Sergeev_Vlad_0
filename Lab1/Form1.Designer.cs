9aa33c39be47259364f7887af62dea97eb95ef8f
vladserheev <sergeev.vladyslav@gmail.com>
2022-06-01 15:57:57 +0300

    uploading files to dev branch

---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gray;
+            this.button1.Location = new System.Drawing.Point(260, 48);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 78);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Кнопка";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Blue;
+            this.button2.Location = new System.Drawing.Point(260, 216);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 78);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Тоже кнопка";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(74, 71);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 28);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(74, 239);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(124, 28);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 80);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 22);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "x=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 247);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 22);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "z=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(486, 80);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 22);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Результат:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(486, 247);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 22);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Результат:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(594, 80);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 22);
+            this.label5.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(594, 247);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 22);
+            this.label6.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(45, 339);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(171, 113);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Завершити виконання";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 619);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Lab1/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static void makeRandomButtonColor(Button button)
+        {
+            Random rand = new Random();
+
+            switch (rand.Next(0, 8))
+            {
+                case 0: button.BackColor = Color.Yellow; break;
+                case 1: button.BackColor = Color.Blue; break;
+                case 2: button.BackColor = Color.Green; break;
+                case 3: button.BackColor = Color.Red; break;
+                case 4: button.BackColor = Color.White; break;
+                case 5: button.BackColor = Color.Cyan; break;
+                case 6: button.BackColor = Color.DarkBlue; break;
+                case 7: button.BackColor = Color.DarkCyan; break;
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            makeRandomButtonColor(button2);
+            
+            if (button2.BackColor == Color.Cyan)
+            {
+                string firstInputVal = textBox1.Text;
+
+                if (textBox1.Text.Length == 0)
+                {
+                    label5.Text = "Помилка! Пусте значення х";
+                    return;
+                }
+
+                bool isNumericX = int.TryParse(firstInputVal, out _);
+                if (!isNumericX)
+                {
+                    label5.Text = "Помилка! Потрібно вводити тільки цифри";
+                    return;
+                }
+
+                int x = Int32.Parse(firstInputVal);
+
+                // ОДЗ
+                if(x > -2.236 && x < 2.236)
+                {
+                    label5.Text = "Помилка! Недопустиме значення х";
+                    return;
+                }
+
+                double y = Math.Sqrt(Math.Log(((x ^ 2) - 4), Math.E));
+                label5.Text = y.ToString("0.000");
+            }
+            
+
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            makeRandomButtonColor(button1);
+            if (button1.BackColor == Color.DarkBlue)
+            {
+                string firstInputVal = textBox1.Text;
+                string secondInputVal = textBox2.Text;
+
+                if (textBox1.Text.Length == 0)
+                {
+                    label6.Text = "Помилка! Пусте значення х";
+                    return;
+                    
+                }else if (textBox2.Text.Length == 0)
+                {
+                    label6.Text = "Помилка! Пусте значення z";
+                    return;
+                }
+
+                bool isNumericX = int.TryParse(firstInputVal, out _);
+                bool isNumericZ = int.TryParse(secondInputVal, out _);
+                if (!isNumericX || !isNumericZ)
+                {
+                    label6.Text = "Помилка! Потрібно вводити тільки цифри";
+                    return;
+                }
+
+                int x = Int32.Parse(firstInputVal);
+                int z = Int32.Parse(secondInputVal);
+
+                // ОДЗ
+                if(z == 2)
+                {
+                    label6.Text = "Помилка! Недопустиме значення z";
+                    return;
+                }
+
+                double y = x * Math.Atan(z) + Math.Pow(Math.E, (-x - 3) / (z - 2));
+                label6.Text = y.ToString("0.000");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Завершити виконання??");
+            Application.Exit();
+        }
+    }
+}
